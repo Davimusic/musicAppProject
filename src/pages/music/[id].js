9a572b38c2601f0f4b7c331d264fd8cfbbabc459
webnocode <gdfajardo6@soy.sena.edu.co>
2025-01-29 21:58:12 -0500

    29enero-4

--- src/pages/music/[id].js
+++ src/pages/music/[id].js
@@ -1,49 +1,89 @@
 "use client"
+import React, { useEffect, useState } from 'react';
 import Audio from "@/components/simple/audio";
-import Image from "@/components/simple/image";
-import Text from "@/components/simple/text";
+import ImageAndText from "@/components/complex/imageAndText ";
+
+const test1 = [
+    {
+      "image": {
+        "className": [],
+        "style": { "backgroundColor": "transparent" },
+        "alt": "Imagen 1",
+        "width": 50,
+        "height": 50,
+        "id": "ima1",
+        "src": "https://res.cloudinary.com/dplncudbq/image/upload/v1693680649/video_u9gl6j.png"
+      },
+      "text": {
+        "id": "text1",
+        "text": "Texto 1",
+        "style": {},
+        "className": []
+      }
+    }
+]
+
+
 
 export default function Music() {
+
+    const is = [
+        {
+          "image": {
+            "onClick": () => setSong('https://res.cloudinary.com/dplncudbq/video/upload/v1699541461/n11_dcpomq.mp3'),
+            "className": [],
+            "style": { "backgroundColor": "transparent" },
+            "alt": "Imagen 1",
+            "width": 50,
+            "height": 50,
+            "id": "ima1",
+            "src": "https://res.cloudinary.com/dplncudbq/image/upload/v1693680649/video_u9gl6j.png"
+          },
+          "text": {
+            "id": "text1",
+            "text": "Texto 1",
+            "style": {},
+            "className": []
+          }
+        },
+        {
+          "image": {
+            "className": [],
+            "style": { "backgroundColor": "transparent" },
+            "alt": "Imagen 2",
+            "width": 50,
+            "height": 50,
+            "id": "ima2",
+            "src": "https://res.cloudinary.com/dplncudbq/image/upload/v1693680649/video_u9gl6j.png",
+            "onClick": () => setSong('https://res.cloudinary.com/dplncudbq/video/upload/v1699541446/n12_sk8ytz.mp3'),
+          },
+          "text": {
+            "id": "text2",
+            "text": "Texto 2",
+            "style": {},
+            "className": []
+          }
+        },
+    ]
+    const [song, setSong] = useState('https://res.cloudinary.com/dplncudbq/video/upload/v1692977795/mias/relax7_orxvbj.mp3'); 
+    
+
     return (
-      <div style={{ height: '100vh', background: 'red', display: 'flex', flexDirection: 'column' }}>
+      <div style={{ height: '100vh', background: 'black', display: 'flex', flexDirection: 'column' }}>
         {/* Contenedor del contenido superior con scroll */}
         <div style={{ flex: 1, overflowY: 'auto' }}>
-          {Array.from({ length: 20 }).map((_, index) => (
-            <div key={index} style={{ display: 'flex', alignItems: 'flex-end', marginBottom: '10px' }}>
-              <Image
-                onClick={() => alert('si')}
-                className={[]}
-                style={{ backgroundColor: 'transparent' }}
-                alt={'sis'}
-                width={50}
-                height={50}
-                id={'ima'}
-                src={'https://res.cloudinary.com/dplncudbq/image/upload/v1693680649/video_u9gl6j.png'}
-              />
-              <Text id={'textau'} text={`hola ${index + 1}`} />
-            </div>
-          ))}
+          <ImageAndText content = {is}/>
         </div>
   
         {/* Contenedor fijo en la parte inferior */}
         <div style={{ backgroundColor: '#1e1e1e', padding: '10px' }}>
           <div style={{ display: 'flex', alignItems: 'flex-end' }}>
-            <Image
-              onClick={() => alert('si')}
-              className={[]}
-              style={{ backgroundColor: 'transparent' }}
-              alt={'sis'}
-              width={50}
-              height={50}
-              id={'ima'}
-              src={'https://res.cloudinary.com/dplncudbq/image/upload/v1693680649/video_u9gl6j.png'}
-            />
-            <Text id={'textau'} text={'hola'} />
+            <ImageAndText content={test1}/>
           </div>
           <Audio
             id={'1'}
-            src={'https://res.cloudinary.com/dplncudbq/video/upload/v1692977795/mias/relax7_orxvbj.mp3'}
-            autoPlay={true}
+            src={song}
+            autoPlay={false}
             loop={false}
             controlsList={true}
             backgroundColor="#1e1e1e" // Fondo oscuro
