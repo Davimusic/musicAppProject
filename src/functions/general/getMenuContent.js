dea31a560b91e99a22a734ffb03151aaab36d0dd
webnocode <gdfajardo6@soy.sena.edu.co>
2025-01-29 19:19:48 -0500

    29enero

--- src/functions/general/getMenuContent.js
+++ src/functions/general/getMenuContent.js
@@ -6,8 +6,6 @@ export default function getMenuContent(){
     const router = useRouter();
 
     return( <>
-                <button className="botones" onClick={() => { window.location.href = 'https://apu-kappa.vercel.app/' }}>APU</button>
-                <button className="botones" onClick={() => { window.location.href = 'https://gannt-blush.vercel.app/' }}>GANNT</button>
                 <button className="botones" onClick={handleMultipleFunctions(
                 () => localStorageAcces('POST', 'loggingStatus', false),                     
                 () => router.push('/')
